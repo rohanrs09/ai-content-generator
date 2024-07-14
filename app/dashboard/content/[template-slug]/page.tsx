95062c4938443198259bb27645ab63e25549f511
rohanshelke <rohan_shelke_it@moderncoe.edu.in>
2024-07-14 21:39:47 +0530

    get response to gemini and show it in output display

--- app/dashboard/content/[template-slug]/page.tsx
+++ app/dashboard/content/[template-slug]/page.tsx
@@ -1,6 +1,6 @@
 "use client"
 
-import React from "react";
+import React, { useState } from "react";
 import FormSection from "../_component/FormSection";
 import OutputSection from "../_component/OutputSection";
 import { TEMPLATE } from "../../_components/TemplateListSection";
@@ -8,6 +8,7 @@ import Templates from "@/app/(data)/Templates";
 import { ArrowLeft } from "lucide-react";
 import { Button } from "@/components/ui/button";
 import Link from "next/link";
+import { chatSession } from "@/utils/AiModal";
 
 interface PROPS {
   params: {
@@ -20,8 +21,20 @@ function CreateNewContent(props: PROPS) {
     (item) => item.slug == props.params["template-slug"]
   );
 
-  const GenerateAIContent=(formData:any)=>{
+  const [loading,setLoading]=useState(false);
 
+  const [aiOutput,setAiOutput]=useState<string>(''); 
+
+  const GenerateAIContent=async(formData:any)=>{
+      setLoading(true);
+      const SelectedPrompt=selectedTemplate?.aiPrompt;
+      const FinalAIPrompt = JSON.stringify(formData)+", "+SelectedPrompt;
+
+      const result=await chatSession.sendMessage(FinalAIPrompt);
+
+      console.log(result.response.text());
+      setAiOutput(result.response.text());
+      setLoading(false);
   }
 
   return (
@@ -32,11 +45,12 @@ function CreateNewContent(props: PROPS) {
       <div className="grid grid-cols-1 md:grid-cols-3 gap-5 py-5">
       {/* formSection */}
       <FormSection selectedTemplate={selectedTemplate}
-      userFormInput={(v:any)=>console.log(v)}
+      userFormInput={(v:any)=>GenerateAIContent(v)}
+      loading={loading}
       />
       {/* outputSection */}
       <div className="col-span-2">
-      <OutputSection />
+      <OutputSection aiOutput={aiOutput} />
       </div>
     </div>
     </div>
