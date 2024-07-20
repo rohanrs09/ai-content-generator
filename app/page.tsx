e8cccf7076f198a36b0d24dbfac974847685d8c8
rohanshelke <rohan_shelke_it@moderncoe.edu.in>
2024-07-20 10:50:39 +0530

    route navigate to dashboard page

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,18 @@
+"use client"
+
 import { Button } from "@/components/ui/button";
 import Image from "next/image";
+import { useRouter } from "next/navigation";
 
 export default function Home() {
+
+  const router = useRouter()
+
+
   return (
     <div>
       <h2>Home page</h2>
-      <Button>Subscribe</Button>
+      <Button type="button" onClick={() => router.push('/dashboard')}>Subscribe</Button>
     </div>
   );
 }
